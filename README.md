807bb68b458dd60ff21402929d93cb3115199241
Erick Palombo <erick_palombo_@hotmail.com>
2023-05-18 18:16:42 -0300

    adicionado seeds e readme

--- README.md
+++ README.md
@@ -1,24 +1,79 @@
-# README
+<h1>AuctionHouse</h1> 
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+<p align="center">
+  <img src="http://img.shields.io/static/v1?label=Ruby&message=3.2.2&color=red&style=for-the-badge&logo=ruby"/>
+  <img src="http://img.shields.io/static/v1?label=Ruby%20On%20Rails%20&message=7.0.4.3&color=red&style=for-the-badge&logo=ruby"/>
+   <img src="http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=RED&style=for-the-badge"/>
+</p>
 
-Things you may want to cover:
+> Status do Projeto: ⚠️ Projeto em Desenvolvimento
 
-* Ruby version
+## Descrição do projeto 
 
-* System dependencies
+<p align="justify">
+  Neste projeto foi feito uma aplicação para simular um sistema de leilão. 
+</p>
 
-* Configuration
+## Funcionalidades
 
-* Database creation
+✔️ Cadastro de Itens para Leilão
 
-* Database initialization
+✔️ Cadastro de Lotes para Leilão
 
-* How to run the test suite
+✔️ Visualizar Lotes
 
-* Services (job queues, cache servers, search engines, etc.)
+✔️ Fazer Lances
 
-* Deployment instructions
+✔️ Resultado de um Leilão
 
-* ...
+
+## Pré-requisitos
+
+⚠️ [Ruby](https://www.ruby-lang.org/pt/downloads/)
+
+No terminal, clone o projeto: 
+
+```
+git clone git@github.com:Degorous/td10_auctionhouse_app.git
+```
+
+No terminal, abra a pasta do projeto e digite:
+
+```
+bin/setup
+```
+
+## Como rodar os testes
+
+No terminal, abra a pasta do projeto e digite:
+
+```
+$ rspec
+```
+
+## Como executar o app
+
+No terminal e dentro da página do projeto, digite:
+
+```
+$ rails server
+```
+
+No navegador, digite:
+
+```
+http://localhost:3000/
+```
+
+## Tarefas em aberto
+
+📝 Algumas validações de segurança ficaram pendentes
+
+📝 
+
+📝 
+
+### Participante: 
+|name|email|present|
+| -------- | -------- | -------- |
+|Erick Palombo|erick_palombo_@hotmail.com|true|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Contas de usuários
+
+admin_1 = User.create!(email: 'andre@leilaodogalpao.com.br', cpf: '025.703.680-67', password: 'password')
+admin_2 = User.create!(email: 'joao@leilaodogalpao.com.br', cpf: '821.528.250-47', password: 'password')
+user_1 = User.create!(email: 'carla@mail.com', cpf: '504.054.690-47', password: 'password')
+user_2 = User.create!(email: 'bruna@mail.com', cpf: '850.373.260-28', password: 'password')
+
+# Cadastro de itens
+periferico = Category.create!(name: 'Periférico')
+hardware = Category.create!(name: 'Hardware')
+smartphone = Category.create!(name: 'Smartphone')
+
+item_a = Item.create!(name: 'G-413 CARBON', description: 'Teclado Gamer', weight: '1105', width: '445',
+                      height: '132', depth: '34', category: periferico)
+item_b = Item.create!(name: 'G-933 Artemis', description: 'Fone Gamer', weight: '336', width: '81',
+                      height: '172', depth: '182', category: periferico)
+item_c = Item.create!(name: 'G-440', description: 'Mouse Gamer', weight: '229', width: '340',
+                      height: '280', depth: '3', category: periferico)
+item_d = Item.create!(name: 'C922 WEBCAM', description: 'Webcam para stream', weight: '162', width: '95',
+                      height: '44', depth: '71', category: periferico)
+item_e = Item.create!(name: 'YETI', description: 'Microfone USB', weight: '550', width: '12',
+                      height: '29', depth: '12', category: periferico)
+item_f = Item.create!(name: 'SSD 240G Kingston', description: 'SSD da Kingston', weight: '40', width: '70',
+                      height: '7', depth: '100', category: hardware)
+item_g = Item.create!(name: 'Memória Kingston Fury 8GB', description: 'Memória DDR4 Kingston', weight: '50', width: '134',
+                      height: '7', depth: '34', category: hardware)
+item_h = Item.create!(name: 'MSI MAG A650BN', description: 'Fonte da MSI', weight: '2060', width: '150',
+                      height: '86', depth: '140', category: hardware)
+item_i = Item.create!(name: 'Moto G52', description: 'Smartphone da Motorola', weight: '169', width: '74',
+                      height: '160', depth: '8', category: smartphone)
+item_j = Item.create!(name: 'Galaxy A23', description: 'Smartphone da Samsung', weight: '290', width: '77',
+                      height: '165', depth: '8', category: smartphone)
+
+# Criando lotes
+
+# Lote em andamento com dois itens
+lot_1 = Lot.create!(code: 'ABC123456', start_date: Date.current, finish_date: 1.day.from_now.to_date,
+                    start_bid: 100, increase_bid: 10, status: 5, started_by: admin_1, finished_by: admin_2)
+LotItem.create!(item: item_h, lot: lot_1)
+LotItem.create!(item: item_d, lot: lot_1)
+
+# Lote pendente sem itens
+lot_2 = Lot.create!(code: 'ZSE892309', start_date: Date.current, finish_date: 1.week.from_now.to_date,
+                    start_bid: 100, increase_bid: 10, status: 0, started_by: admin_1)
+
+# Lote pendente com dois itens
+lot_3 = Lot.create!(code: 'OPA140886', start_date: Date.current, finish_date: 1.week.from_now.to_date,
+                    start_bid: 100, increase_bid: 10, status: 0, started_by: admin_1)
+LotItem.create!(item: item_c, lot: lot_3)
+LotItem.create!(item: item_i, lot: lot_3)
+
+travel_to 1.month.ago do
+
+  # Lote com data expirada que recebeu um lance
+  lot_4 = Lot.create!(code: 'JXP357408', start_date: Date.current, finish_date: 1.week.from_now.to_date,
+                      start_bid: 100, increase_bid: 10, status: 5, started_by: admin_1, finished_by: admin_2, bid: 105, bid_user_id: user_1.id)
+  LotItem.create!(item: item_a, lot: lot_4)
+  LotItem.create!(item: item_e, lot: lot_4)
+  
+  # Lote com data expirada sem lance
+  lot_5 = Lot.create!(code: 'NYX923646', start_date: Date.current, finish_date: 1.week.from_now.to_date,
+                      start_bid: 100, increase_bid: 10, status: 5, started_by: admin_1, finished_by: admin_2)
+  LotItem.create!(item: item_b, lot: lot_5)
+  LotItem.create!(item: item_f, lot: lot_5)
+end
